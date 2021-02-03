a6acc80c406aa9c553ac60e9a816fcd0f3928de4
K-Zimny <kenneth.zimny@gmail.com>
2021-02-03 16:30:21 -0600

    Edited styles

--- front-page.php
+++ front-page.php
@@ -16,12 +16,12 @@ get_header(); ?>
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
-					<a href="" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a>
-					<a href="" class="soc-icon fb"><span class="screen-reader-text">Facebook</span></a>
-					<a href="" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
-					<a href="" class="soc-icon db"><span class="screen-reader-text">Dribble</span></a>
-					<a href="" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
-					<a href="" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a>
+					<!-- <a href="" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a> -->
+					<!-- <a href="" class="soc-icon fb"><span class="screen-reader-text">Facebook</span></a> -->
+					<a href="https://www.linkedin.com/in/kenneth-zimny/" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
+					<!-- <a href="" class="soc-icon db"><span class="screen-reader-text">Dribble</span></a> -->
+					<a href="https://github.com/K-Zimny" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
+					<!-- <a href="" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a> -->
 				</div>
 				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
 			<?php endwhile; ?>
